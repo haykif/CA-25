584492137d20cfdf385ff7b20e29c36530161c80
raph7413 <raphael.rodriguez74@gmail.com>
2025-04-08 17:11:28 +0200

    Update CapteurMouvement.py

--- python/CapteurMouvement.py
+++ python/CapteurMouvement.py
@@ -1,11 +1,22 @@
-from gpiozero import MotionSensor
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
+# Configuration des GPIO
+PIR_PIN = 26  # GPIO 17 correspond à la Pin 11
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIR_PIN, GPIO.IN)
 
-pir = MotionSensor(17)
-while True:
-    pir.wait_for_motion()
-    print("bougé!")
-    pir.wait_for_no_motion()
-    print("pas bougé")
+print("✅ Capteur PIR prêt ! Attente de mouvement...")
 
+try:
+    while True:
+        if GPIO.input(PIR_PIN):  # Si un mouvement est détecté
+            print("⚠️ Mouvement détecté !")
+        else:
+            print("aucun mouvement detecter")
+
+        time.sleep(1)  # Petite pause pour éviter les détections trop rapides
+
+except KeyboardInterrupt:
+    print("❌ Arrêt du programme")
+    GPIO.cleanup()  # Nettoyage des GPIO
