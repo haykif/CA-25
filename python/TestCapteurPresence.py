383ce7581492338dbbb9ee06f4694dc2fea46055
raph7413 <raphael.rodriguez74@gmail.com>
2025-05-17 10:43:44 +0200

    Update CapteurPresence.py

--- python/TestCapteurPresence.py
+++ python/TestCapteurPresence.py
@@ -7,15 +7,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 print("⏳ Initialisation du capteur PIR...")
-time.sleep(2)  # Temps pour que le capteur se stabilise
+time.sleep(15)  # Temps pour que le capteur se stabilise
 print("✅ Prêt ! Surveillance du mouvement...")
 
 try:
     while True:
         if GPIO.input(PIR_PIN):
             print("⚠️ Mouvement détecté !")
-        else:
-            print("✅ Aucun mouvement.")
+        #else:
+            #print("✅ Aucun mouvement.")
         time.sleep(1)
 
 except KeyboardInterrupt:
